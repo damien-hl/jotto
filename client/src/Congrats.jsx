54ac6eeceda688a21f016590dc6ee8828b6fb704
Damien Heulin <damienheulin87@gmail.com>
2021-05-14 13:08:56 +0200

    Test and code for Congrats component

---
+++ client/src/Congrats.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+/**
+ * Functionnal React component for congratulatory message
+ * 
+ * @function Congrats
+ * @param {{success: boolean}} props React props 
+ * @returns {JSX.Element} Rendered component (or null if `success` prop is false)
+ */
+function Congrats({ success }) {
+    if (success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        );
+    } else {
+        return (
+            <div data-test="component-congrats" />
+        );
+    }
+}
+
+export default Congrats;

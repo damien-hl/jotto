d9de9a142d1cde8cf47ceb588e8bac3ba076fa32
Damien Heulin <damienheulin87@gmail.com>
2021-05-14 21:37:28 +0200

    Test and code for GuessedWords

--- client/src/Congrats.test.jsx
+++ client/src/Congrats.test.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import Enzyme, { ShallowWrapper } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import { checkProps, findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 

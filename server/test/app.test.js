793faea3a57e919a67f0829b1ee05078ad3ac9d7
Damien Heulin <damienheulin87@gmail.com>
2021-05-15 20:44:20 +0200

    Added Prettier and lint actions

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -4,25 +4,25 @@ const createApp = require("../app.js");
 let fastify;
 
 describe("Test server", () => {
-    beforeEach(async () => {
-        fastify = createApp();
-        await fastify.ready();
-        global.agent = request.agent(fastify.server);
-    });
+  beforeEach(async () => {
+    fastify = createApp();
+    await fastify.ready();
+    global.agent = request.agent(fastify.server);
+  });
 
-    afterEach(async () => {
-        await fastify.close();
-    });
+  afterEach(async () => {
+    await fastify.close();
+  });
 
-    test("responds with status 200 the GET method", async () => {
-        const response = await request(fastify.server).get("/");
+  test("responds with status 200 the GET method", async () => {
+    const response = await request(fastify.server).get("/");
 
-        expect(response.statusCode).toBe(200);
-    });
+    expect(response.statusCode).toBe(200);
+  });
 
-    test("response is a five letter word", async () => {
-        const response = await request(fastify.server).get("/");
+  test("response is a five letter word", async () => {
+    const response = await request(fastify.server).get("/");
 
-        expect(response.text.length).toBe(5);
-    });
+    expect(response.text.length).toBe(5);
+  });
 });

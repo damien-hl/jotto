8b2d95d8b78c68e27c75f2d52125f649aee0b763
Damien Heulin <damienheulin87@gmail.com>
2021-05-17 11:50:51 +0200

    Setup Redux

---
+++ client/src/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ *
+ * @param {Array} state Array of guessed words
+ * @param {object} action Action to be reduced
+ * @returns {boolean}
+ */
+// eslint-disable-next-line no-unused-vars
+export default (state, action) => {
+  return null;
+};

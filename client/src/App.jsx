d5bbf6bba0d9c5263616472acf856b12fd05cc5b
Damien Heulin <damienheulin87@gmail.com>
2021-05-19 19:49:19 +0200

    Refactored App state with useReducer hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
 import Input from "./Input";
 import { getSecretWord } from "./actions";
 import "./App.css";
+
+/**
+ * Reducer to update state, automatically called by dispatch
+ *
+ * @function reducer
+ * @param {object} state previous state
+ * @param {object} action A type prop and a payload
+ * @returns {object} New state
+ */
+const reducer = (state, action) => {
+  switch (action.state) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+};
 
 /**
  * Main React component
@@ -12,10 +29,15 @@ import "./App.css";
  * @returns {JSX.Element} Rendered App component
  */
 function App() {
-  const [secretWord, setSecretWord] = useState("");
-  // TODO: Get props from shared state
+  // const [secretWord, setSecretWord] = useState("");
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: "" });
+
   const success = false;
   const guessedWords = [];
+
+  const setSecretWord = (secretWord) => {
+    dispatch({ type: "setSecretWord", payload: secretWord });
+  };
 
   useEffect(() => {
     getSecretWord(setSecretWord);
@@ -25,7 +47,7 @@ function App() {
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
       <Congrats success={success} />
-      <Input success={success} secretWord={secretWord} />
+      <Input success={success} secretWord={state.secretWord} />
       <GuessedWords guessedWords={guessedWords} />
     </div>
   );
